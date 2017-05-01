0d22b8be1b988a2e70eddd2b6bd8ecf78867301b
Johan Brattemark <johan.brattemark@gmail.com>
2017-04-30 20:31:28 -0700

    Add event listeners to each color picker in palette, to implement recommended step 5

--- pix.js
+++ pix.js
@@ -1,31 +1,33 @@
 window.onload = function() {
+  let colors = ['red', 'blue'];
+  let selectedColor = colors[0];
+
+  // Drawing grid
   let grid = document.createElement('div');
   grid.id = 'grid';
-
   for (let row = 0; row < 2; row++) {
     for (let col = 0; col < 2; col++) {
       let pixel = document.createElement('div');
       pixel.className = 'pixel';
       pixel.addEventListener('click', event => {
-        pixel.style.backgroundColor = 'red';
+        pixel.style.backgroundColor = selectedColor;
       });
       grid.append(pixel);
     }
   }
   document.body.append(grid);
 
+  // Palette
   let palette = document.createElement('div');
   palette.id = 'palette';
-  let colors = ['red', 'blue'];
   for (color of colors) {
     let colorSource = document.createElement('div');
     colorSource.className = 'colorSource';
     colorSource.style.backgroundColor = color;
+    colorSource.addEventListener('click', event => {
+      selectedColor = colorSource.style.backgroundColor;
+    });
     palette.append(colorSource);
   }
   document.body.append(palette);
 }
-
-
-// 4.Create a small palette of two colors (e.g. red and blue)
-// below the canvas using more div tags.
