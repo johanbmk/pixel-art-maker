21d916a4bf682b9c7a29a07433ab58c808b47f11
Johan Brattemark <johan.brattemark@gmail.com>
2017-04-30 19:46:46 -0700

    Add palette for picking colors, to implement recommended step 4

--- pix.js
+++ pix.js
@@ -12,6 +12,20 @@ window.onload = function() {
       grid.append(pixel);
     }
   }
-
   document.body.append(grid);
+
+  let palette = document.createElement('div');
+  palette.id = 'palette';
+  let colors = ['red', 'blue'];
+  for (color of colors) {
+    let colorSource = document.createElement('div');
+    colorSource.className = 'colorSource';
+    colorSource.style.backgroundColor = color;
+    palette.append(colorSource);
+  }
+  document.body.append(palette);
 }
+
+
+// 4.Create a small palette of two colors (e.g. red and blue)
+// below the canvas using more div tags.
